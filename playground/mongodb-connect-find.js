33d04bda8095a4de54756c5af135d389e9b72e18
devonbahary <devonbahary@gmail.com>
2018-03-18 16:08:32 -0700

    Initial commit

---
+++ playground/mongodb-connect-find.js
@@ -0,0 +1,59 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+// MongoDB module v2
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server.');
+    }
+    console.log('Connected to MongoDB server.');
+
+    // finding a document in 'Todos' with particular ObjectID
+    db.collection('Todos').find({
+        _id: new ObjectID('5aaee9301fa0fe8ee1be2af4')
+    }).toArray().then((docs) => {
+        console.log('Todos');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });
+
+    // counting the number of documents in 'Todos'
+    db.collection('Todos').find().count().then((count) => {
+        console.log(`Todos count: ${count}`);
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });
+
+    // finding documents in 'Users' with name 'Devon'
+    db.collection('Users').find({name: 'Devon'}).toArray().then((docs) => {
+        console.log('Results:');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch users', err);
+    });
+
+    // db.close();
+});
+
+// MongoDB module v3
+// MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+//     if (err) {
+//         return console.log('Unable to connect to MongoDB server.');
+//     }
+//     console.log('Connected to MongoDB server.');
+//     const db = client.db('TodoApp');
+//
+//     db.collection('Todos').insertOne({
+//         text: 'Something to do',
+//         completed: false
+//     }, (err, result) => {
+//         if (err) {
+//             return console.log('Unable to insert todo', err);
+//         }
+//
+//         console.log(JSON.stringify(result.ops, undefined, 2));
+//     });
+//
+//     client.close();
+// });
